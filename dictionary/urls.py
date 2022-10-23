44c4b41782c25c606fac46c29c684deee11c40a3
David Ekong <davidukemeekong1@gmail.com>
2022-10-23 20:38:09 +0100

    The main structure of the dictionary

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('search/', views.home, name='search')
+    path('search/', views.home, name='search'), 
+    path('output/<word>', views.output, name='output')
 ]
--- dictionary/views.py
+++ dictionary/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
-
+import difflib
+from django.shortcuts import redirect, render
+from PyDictionary import PyDictionary
+from english_words import english_words_set
 # Create your views here.
 def home(request):
+    if request.method == "GET":
+        if 'search_button' in request.GET:
+            word = request.GET.get('word')
+            return redirect('output', word)
     return render(request, 'dictionary/dict.html')
+
+def output(request, word):
+    dictionary = PyDictionary()
+    meanings = dictionary.meaning(difflib.get_close_matches(word, english_words_set)[0])
+    new_list = []
+    if meanings is not None:
+        for key, value in meanings.items():
+            new_list.append((key, value))
+    context = {
+        'suggested': list(difflib.get_close_matches(word, english_words_set)), 
+        'word': word, 
+        'list': new_list, 
+        'meanings': meanings
+    }
+    print(context['suggested'])
+    return render(request, 'dictionary/output.html', context)
