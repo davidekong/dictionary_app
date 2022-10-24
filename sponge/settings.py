66f80e8198a1fc3aac8e87fa6efe48726b0f17eb
David Ekong <davidukemeekong1@gmail.com>
2022-10-24 17:21:22 +0100

    changes made

--- sponge/settings.py
+++ sponge/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'dictionary',
-    'PyDictionary'
+    'PyDictionary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
